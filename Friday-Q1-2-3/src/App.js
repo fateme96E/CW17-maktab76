d7a24446e7d36bcecb47e1fe5334c61fce878772
Fateme Esmaili <92683438+fateme96E@users.noreply.github.com>
2022-07-22 15:04:39 -0700

    Add files via upload

---
+++ Friday-Q1-2-3/src/App.js
@@ -0,0 +1,12 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import RectangleArea from './Components/RectangleArea/RectangleArea';
+
+function App() {
+  return (
+    <div className="App">
+      <RectangleArea />
+    </div>
+  );
+}
+
+export default App;
